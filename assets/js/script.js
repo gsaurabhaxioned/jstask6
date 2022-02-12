6e4fdeb848a329435947926d665cbf6c7c051aac
saurabh-gaonkar <saurabhg@axioned.com>
2022-02-12 21:10:40 +0530

    made js code short

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,11 @@
 const content_box = document.querySelectorAll('.content');
 
-for (let i = 0; i < content_box.length; i++) {
-    if (content_box[i].offsetTop < window.scrollY + (window.screen.height / 1.5)) {
-        content_box[i].style.transform = "translateX(0)";
-    }
-
-}
-window.onscroll = function () {
+function loadOnScroll() {
     for (let i = 0; i < content_box.length; i++) {
         if (content_box[i].offsetTop < window.scrollY + (window.screen.height / 1.5)) {
             content_box[i].style.transform = "translateX(0)";
         }
     }
-
 }
+window.onload = loadOnScroll;
+window.onscroll = loadOnScroll;
