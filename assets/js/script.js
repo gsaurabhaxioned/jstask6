3bdd2ef17f7d0e481827e25fd526a5d717848a62
saurabh-gaonkar <saurabhg@axioned.com>
2022-02-12 16:01:37 +0530

    js code started

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,11 @@
-/* Author: 
+const content_box = document.querySelectorAll('.content'),
+    window_height = window.screen.height;
 
-*/
+window.onscroll= function(){
+    for(let i=0;i<content_box.length;i++){
+    if(content_box[i].offsetTop < window_height){
+        content_box[i].style.transform="translateX(0)";
+    }
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
