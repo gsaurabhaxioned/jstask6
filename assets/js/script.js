d710f5bf3995a2b95809f429bb65e7ffab3d6097
saurabh-gaonkar <saurabhg@axioned.com>
2022-02-12 21:04:19 +0530

    javascript code fomatted

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,16 @@
 const content_box = document.querySelectorAll('.content');
-    
-for(let i=0;i<content_box.length;i++){
-    if(content_box[i].offsetTop < window.scrollY + (window.screen.height/1.5)){
-        content_box[i].style.transform="translateX(0)";
+
+for (let i = 0; i < content_box.length; i++) {
+    if (content_box[i].offsetTop < window.scrollY + (window.screen.height / 1.5)) {
+        content_box[i].style.transform = "translateX(0)";
     }
 
 }
-window.onscroll= function(){
-    for(let i=0;i<content_box.length;i++){
-    if(content_box[i].offsetTop < window.scrollY + (window.screen.height/1.5)){
-        content_box[i].style.transform="translateX(0)";
+window.onscroll = function () {
+    for (let i = 0; i < content_box.length; i++) {
+        if (content_box[i].offsetTop < window.scrollY + (window.screen.height / 1.5)) {
+            content_box[i].style.transform = "translateX(0)";
+        }
     }
-}
- 
+
 }
