7003d11227ad9f0b7fe418bc64e5a6b32c280175
saurabh-gaonkar <saurabhg@axioned.com>
2022-02-15 19:04:20 +0530

    js code added for reverse effect

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,13 @@ function loadOnScroll() {
     for (let i = 0; i < content_box.length; i++) {
         if (content_box[i].offsetTop < window.scrollY + (window.screen.height / 1.5)) {
             content_box[i].style.transform = "translateX(0)";
+        } else {
+            if (i % 2 == 0) {
+                content_box[i].style.transform = "translateX(1000px)";
+            } else {
+                content_box[i].style.transform = "translateX(-1000px)";
+            }
+
         }
     }
 }
